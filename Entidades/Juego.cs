37c38197cf97eca582cd7913c0626e237289b8a9
Martin Bianchi <tinchola7@gmail.com>
2018-05-31 10:58:24 -0300

    Add files via upload

---
+++ Entidades/Juego.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Juego
+    {
+        #region Fields
+
+        private string _palabraAAdivinar;
+
+        public string PalabraAAdivinar
+        {
+            get { return _palabraAAdivinar; }
+            set { _palabraAAdivinar = value; }
+        }
+
+        private char[] _palabraEscondida;
+
+        public char[] PalabraEscondida
+        {
+            get { return _palabraEscondida; }
+            set { _palabraEscondida = value; }
+        }
+
+        #endregion
+
+        #region Methods
+        public string IniciarJuego()
+        {
+            return "Juego Iniciado";
+        }
+
+        public string DevolverPalabraParcial()
+        {
+            return new string(PalabraEscondida);
+        }
+
+        public bool IngresarLetra(char letra)
+        {
+            int posicion = PalabraAAdivinar.IndexOf(letra);
+
+            if (posicion != -1)
+            {
+                PalabraEscondida[posicion] = letra;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AdivinoPalabra()
+        {
+            string pal2 = new string (PalabraEscondida);
+            return pal2.Equals(PalabraAAdivinar);
+        }
+
+        public void IngresarPalabraAAdivinar(string palabra)
+        {
+            PalabraAAdivinar = palabra;
+
+            PalabraEscondida = new char[PalabraAAdivinar.Length];
+            for (int i = 0; i < PalabraEscondida.Length; i++)
+            {
+                PalabraEscondida[i] = '-';
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Pruebas/TestNuevoJuego.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class TestNuevoJuego
+    {
+        [TestMethod]
+        public void TestIniciarNuevoJuego()
+        {
+            //Arrange
+            Juego juego = new Juego();
+
+            //Act
+            string inicio = juego.IniciarJuego();
+
+            //Assert
+            Assert.AreEqual("Juego Iniciado", inicio);
+        }
+
+        [TestMethod]
+        public void TestDefinirPalabraAAdivinar()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+
+            //Act
+            string palabraAAdivinar = juego.PalabraAAdivinar;
+
+            //Assert
+            Assert.AreEqual("vaso", palabraAAdivinar);
+        }
+
+        [TestMethod]
+        public void TestIngresarLetraCorrecta()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+
+            //Act
+            bool esCorrecta = juego.IngresarLetra('a');
+
+            //Assert
+            Assert.AreEqual(true, esCorrecta);
+        }
+
+        [TestMethod]
+        public void TestIngresarLetraIncorrecta()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+
+            //Act
+            bool esCorrecta = juego.IngresarLetra('b');
+
+            //Assert
+            Assert.AreEqual(false, esCorrecta);
+        }
+
+        [TestMethod]
+        public void TestAdivinoPalabra()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+            juego.IngresarLetra('v');
+            juego.IngresarLetra('a');
+            juego.IngresarLetra('s');
+            juego.IngresarLetra('o');
+
+            //Act
+            bool adivino = juego.AdivinoPalabra();
+
+            //Assert
+            Assert.AreEqual(true, adivino);
+        }
+
+        [TestMethod]
+        public void TestNoAdivinoPalabra()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+            juego.IngresarLetra('v');
+            juego.IngresarLetra('a');
+            juego.IngresarLetra('s');
+
+            //Act
+            bool adivino = juego.AdivinoPalabra();
+
+            //Assert
+            Assert.AreEqual(false, adivino);
+        }
+
+        [TestMethod]
+        public void VerProgresoPalabraSinAciertos()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+            juego.IngresarLetra('x');
+
+            //Act
+            string palabra = juego.DevolverPalabraParcial();
+
+            //Assert
+            Assert.AreEqual("----", palabra);
+        }
+
+        [TestMethod]
+        public void VerProgresoPalabraConAciertos()
+        {
+            //Arrange
+            Juego juego = new Juego();
+            juego.IniciarJuego();
+            juego.IngresarPalabraAAdivinar("vaso");
+            juego.IngresarLetra('v');
+
+            //Act
+            string palabra = juego.DevolverPalabraParcial();
+
+            //Assert
+            Assert.AreEqual("v---", palabra);
+        }
+
+    }
+}
